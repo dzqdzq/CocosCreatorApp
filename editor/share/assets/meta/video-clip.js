12920156bd007ab2aa89c13a2c12e73ee9c0f837
dzqdzq <635587322@qq.com>
2025-12-05 13:45:33 +0800

    Update v2.4.9 from CocosCreatorApp2_4_9

---
+++ editor/share/assets/meta/video-clip.js
@@ -0,0 +1,1 @@
+"use strict";const e=require("./native-asset");module.exports=class extends e{constructor(e){super(e)}static version(){return"1.0.4"}static defaultType(){return"video-clip"}createAsset(){return new cc.VideoClip}import(e,t){super.import(e,t)}};
